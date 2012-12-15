fd131fd160d6f4a3793466ef79cc68ae992874cd
Brendan Wright <bmwtwo@princeton.edu>
2012-12-15 05:23:00 -0500

    Allow entry of numbers into selected square. Begin error checking on rows as the user types.

--- input.js
+++ input.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
+   var selectedBox = null;
+
    $(".largeBox p").on("click", function() {
-      $(this).addClass("selected");
+      toggleSelection(this);
    });
+
+   $(document).on("keydown", function(e) {
+      input(e.keyCode);
+   });
+
+   function toggleSelection(box) {
+      $(selectedBox).removeClass("selected");
+      $(box).addClass("selected");
+      selectedBox = box;
+   }
+
+   function input(keyCode) {
+      if (selectedBox != null && keyCode >= 49 && keyCode <= 57) {
+         $(selectedBox).text(-1 * (48 - keyCode));
+         checkRow(selectedBox);
+      }
+   }
+
+   function checkRow(box) {
+      var row = Math.floor(parseInt($(box).attr("index")) / 9);
+      var value = parseInt($(box).text());
+      for (var x = 9*row; x < 9 * (row+1); x++) {
+         if (x == $(box).attr("index")) continue;
+         if ($('p.[index="' + x + '"]').text() == value) {
+            $(box).addClass("error");
+            return;
+         }
+      }
+      $(box).removeClass("error");
+   }
+
 });
