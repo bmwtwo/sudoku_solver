213819b61ce51ab1a0c683a6a9f2b6835dc92e0c
Brendan Wright <bmwtwo@princeton.edu>
2012-12-15 05:41:55 -0500

    Added error checking for columns and 3x3 boxes.

--- input.js
+++ input.js
@@ -18,18 +18,43 @@ $(document).ready(function() {
    function input(keyCode) {
       if (selectedBox != null && keyCode >= 49 && keyCode <= 57) {
          $(selectedBox).text(-1 * (48 - keyCode));
-         checkRow(selectedBox);
+         checkError(selectedBox);
       }
    }
 
-   function checkRow(box) {
-      var row = Math.floor(parseInt($(box).attr("index")) / 9);
+   function checkError(box) {
       var value = parseInt($(box).text());
+
+      // check row
+      var row = Math.floor(parseInt($(box).attr("index")) / 9);
       for (var x = 9*row; x < 9 * (row+1); x++) {
          if (x == $(box).attr("index")) continue;
          if ($('p.[index="' + x + '"]').text() == value) {
             $(box).addClass("error");
             return;
+         }
+      }
+
+      // check column
+      var col = Math.floor(parseInt($(box).attr("index")) % 9);
+      for (var x = col; x < 9*9 + col; x+=9) {
+         if (x == $(box).attr("index")) continue;
+         if ($('p.[index="' + x + '"]').text() == value) {
+            $(box).addClass("error");
+            return;
+         }
+      }
+
+      // check box
+      var topOfBox = Math.floor(row / 3) * 3;
+      var leftOfBox = Math.floor(col / 3) * 3;
+      for (var r = topOfBox; r < topOfBox + 3; r++) {
+         for (var c = leftOfBox; c < leftOfBox + 3; c++) {
+            if (9*r + c == $(box).attr("index")) continue;
+            if ($('p.[index="' + (9*r + c) + '"]').text() == value) {
+               $(box).addClass("error");
+               return;
+            }
          }
       }
       $(box).removeClass("error");
